1dac181c64fb5f1536ac9679696c35afba1443b1
Jie Yuan <jy2732@columbia.edu>
2017-04-23 21:27:20 +0000

    Parse VCF file into two objects: a dictionary containing SNP info such as position and allele frequency, and a dict of dict containing genotype information.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Linear-Mixed-Model-Imputation
 Perform linear mixed model regression and soft-impute imputation in one step, minimizing error from both operations simultaneously.
+
+Install:
+pip install pyvcf
+pip install pysam
+sudo apt-get install tabix
+
+Setup:
+bgzip file.vcf
+tabix -p vcf file.vcf.gz 
+
--- parse_pyvcf.py
+++ parse_pyvcf.py
@@ -1,13 +1,49 @@
 import sys,argparse
+import vcf
+from pprint import pprint
 
 parser = argparse.ArgumentParser(
                     description="parse input VCF",
                     formatter_class=argparse.RawDescriptionHelpFormatter)
 parser.add_argument('-i', '--input_vcf', help='Input VCF')
 
+def convert_to_matrix(infile):
+    """
+        parse input VCF file into a 2D matrix
+        of users x genotypes. Each entry will
+        have value (0,1,2) for the number of 
+        minor alleles. Additional info is stored
+        in a separate dictionary.
+    """
+    genotypes = {}
+    snp_info = {}
+
+    vcf_reader = vcf.Reader(open(infile, 'r'))
+    count = 0
+    # for now, just take SNPs from chr 7
+    for record in vcf_reader.fetch('7'):
+        count += 1
+        if count % 1000 == 0:
+            print count
+        snp_info[record.ID] = {"chrom":record.CHROM,
+                               "pos":record.POS,
+                               "ref":record.REF,
+                               "alt":record.ALT}
+        user_genos = {}
+        for call in record.samples:
+            if call.called:
+                user_id = call.sample
+                genos = call.data.GT
+                geno_score = float(genos[0]) + float(genos[-1])
+                user_genos[user_id] = geno_score
+        genotypes[record.ID] = user_genos
+    return genotypes, snp_info
+
 def main(args):
     args = parser.parse_args(args)
-    print args.input_vcf
+    genotypes, snp_info = convert_to_matrix(args.input_vcf)
+    pprint(snp_info)
+    pprint(genotypes)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
